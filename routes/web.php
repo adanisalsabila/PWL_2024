72d6bae171affc26901b3d6b36228dcf4c0ebcab
adanisalsabila <adanisalsabilaaa@gmail.com>
2025-02-19 09:28:24 +0700

    percobaan4

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,11 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name='John') {
     return 'Adani Salsabila'.$name;
     });
-    
+
+Route::get('/user/profile', function() {
+    //
+    })->name('profile');
+       
 
 ?>
 
