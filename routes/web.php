125ce932dc7294fea1b3c5dff7e738809fdbdf86
adanisalsabila <adanisalsabilaaa@gmail.com>
2025-02-19 09:29:59 +0700

    percobaan5

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,33 @@ Route::get('/user/{name?}', function ($name='John') {
 Route::get('/user/profile', function() {
     //
     })->name('profile');
-       
+
+//percobaan5
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+    });
+    
 
 ?>
 
